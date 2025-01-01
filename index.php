afb559ef146826338593f7ba8d9ca707b949f687
Bainapalli Pavitro <povitrobainapalli16710@gmail.com>
2025-01-01 14:40:43 +0530

    Website Title Changed
    Website title text changed

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if (isset($_POST['submit']) || isset($_POST['update'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Form</title>
+    <title>User Form - 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
